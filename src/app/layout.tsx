adc08abe31208ba1b5d4dbb6d93127e135531ca5
Marcoouf <marcouf.lebar@gmail.com>
2025-09-11 14:29:44 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 import LatestArticlesRail from '@/components/LatestArticlesRail'
 import ShowOnPath from '@/components/ShowOnPath'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
